12497afaf3d009f723246d29cdcbdc0d842125e5
Creanimo <ferdinand.englaender89@gmail.com>
2021-09-28 18:43:48 +0200

    basic two entry types

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Enqueue child styles.
+ */
+function child_enqueue_styles() {
+	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 100 );
+
+/**
+ * Add custom functions here
+ */
